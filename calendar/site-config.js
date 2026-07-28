7d4bb8095f86df44f78412f6961afe0109db4837
ydh061661 <ydh061661@gmail.com>
2026-07-29 04:49:54 +0900

    Google Analytics 측정 ID 연결

--- calendar/site-config.js
+++ calendar/site-config.js
@@ -4,6 +4,6 @@ window.JLIVE_CONFIG = {
   siteName: "제이라이브 코리아",
   siteUrl: "https://j-live.kr",
   contactEmail: "ydh061661@gmail.com",
-  googleAnalyticsId: "",
+  googleAnalyticsId: "G-QK09RHVF35",
   googleSiteVerification: ""
 };
